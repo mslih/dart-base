d442bdffd39931e3e8b76669c5f1f74829a89f01
misslihui <403065821@qq.com>
2024-08-31 21:13:28 +0800

    数据类型 Number

---
+++ 01_basic/02_数据类型_Number.dart
@@ -0,0 +1,13 @@
+void main() {
+    // num 数字类型（即可以是整数，也可以是小数）
+    num a = 12;
+    num b = 12.12;
+    print(a);
+    print(b);
+    // int 表示整数
+    int c = 12;
+    print(c);
+    // double 表示浮点数 （即可以是整数，也可以是小数）
+    double d = 12.23;
+    print(d);
+}
