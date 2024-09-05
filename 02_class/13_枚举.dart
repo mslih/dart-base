9ae4b0c9ef9abc97bd2217fd76c896fdfc87b950
misslihui <403065821@qq.com>
2024-09-05 23:05:28 +0800

    枚举

---
+++ 02_class/13_枚举.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {}
+
+
+/**
+ * 枚举
+ * 枚举是数量固定的常量值，通过enum关键字声明
+ * enum Color {red, green, blue}
+ * 
+ * 枚举的values常量，可以获取所有枚举值列表
+ * List<Color> colors = Color.values;
+ * 
+ * 可以通过index获取值的索引
+ * print(Color.red.index)
+ */
